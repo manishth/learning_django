a7695bdfd7137036fc2b12a3f4f53ae3a3cb1304
manishth <mthakur970@gmail.com>
2017-01-20 16:29:13 +0530

    view for courses app

--- learning_django/urls.py
+++ learning_django/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from . import views
+from courses import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.hello_world),
+    #url(r'^$', views.hello_world),
     url(r'^course', views.course_list),
 ]
