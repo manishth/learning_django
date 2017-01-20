25cffe041577aec630c02beb1be50171921e3abf
manishth <mthakur970@gmail.com>
2017-01-20 17:37:55 +0530

    new urls file for the courses app and views list comprihension

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from courses.models import Course
+from .models import Course
 
 # Create your views here.
 def course_list(request):
